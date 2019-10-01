8ab9ef99be0d143652fd560150879bf9172fa98b
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2019-09-30 23:26:37 -0500

    Added test cases and a function that fakes a temp dir

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,18 @@
+import contextlib
+import shutil
+import tempfile
 from unittest.mock import Mock, patch
 
 from pybundler import cli
+
+
+@contextlib.contextmanager
+def get_temp_dir():
+    temp_dir = tempfile.mkdtemp()
+    try:
+        yield temp_dir
+    finally:
+        shutil.rmtree(temp_dir)
 
 
 @patch('pybundler.cli.subprocess.run')
@@ -49,3 +61,54 @@ def test_test_release_function(mock_run):
 def test_release_function(mock_run):
     mock_run.return_value = Mock(returncode=0)
     assert cli.release() == 0
+
+
+def test_install_python_version():
+    with get_temp_dir() as dir:
+        def runner(command, cwd):
+            class FakeProcessObject:
+                def __init__(self):
+                    self.returncode = 0
+
+            if command[0] != 'pipenv':
+                raise ValueError("Can only run pipenv args")
+            if command[1] != "--python":
+                raise ValueError("Can only run given flag")
+            if cwd != dir:
+                raise ValueError("Can only run in the given directory")
+
+            fake_process = FakeProcessObject()
+            fake_process.returncode = 1
+            return fake_process
+
+        returncode = cli.install_python_version(
+            pkg_dir=dir,
+            version='3.7.4',
+            runner=runner
+        )
+        assert returncode == 1
+
+
+def test_install_python_default_version():
+    with get_temp_dir() as dir:
+        def runner(command, cwd):
+            class FakeProcessObject:
+                def __init__(self):
+                    self.returncode = 0
+
+            if command[0] != 'pipenv':
+                raise ValueError("Can only run pipenv args")
+            if command[1] != "install":
+                raise ValueError("Can only run valid pipenv installation")
+            if cwd != dir:
+                raise ValueError("Can only run in the given directory")
+
+            fake_process = FakeProcessObject()
+            fake_process.returncode = 1
+            return fake_process
+
+        returncode = cli.install_python_version(
+            pkg_dir=dir,
+            runner=runner
+        )
+        assert returncode == 1
