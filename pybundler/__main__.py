ee110871aac3d4d310500b6c95b9669750434f83
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2019-12-19 22:44:16 -0600

    refactor(cli): add a click group and replace flags with subcommands: pybundler package
    BREAKING CHANGE: The new cli is cleaner beacuse work with flags gets hard and confusing

--- pybundler/__main__.py
+++ pybundler/__main__.py
@@ -13,6 +13,13 @@ This file contains the following functions:
     * enter_path                  - Ask for the path of the new project
     * install_pytest_confirmation - Ask if pytest should be installed
     * main                        - the main function of the script
+
+Commands:
+    build-whell: Creates a source archive and a wheel for your package
+    test-release: Uploads the package to test.pypi.org
+    release: Uploads the pacakge to pypi.org
+    package: Creates a new python package from scratch
+    version: Prints the current version of pybundler
 """
 import os
 
@@ -22,113 +29,19 @@ from colored import fg, attr
 from pybundler import cli, __version__
 
 
-@click.command()
-@click.option('--install-all', is_flag=True,
-              help='Installs all packages from Pipfile')
-@click.option('--install', '-i', default='',
-              help='Install a given package or the content of the Pipfile')
-@click.option('--uninstall', '-u',
-              help='uninstalls a given dependecy')
-@click.option('--dev', '-d', is_flag=True,
-              help='If it is True, install dependency in dev section')
-@click.option('--lock', is_flag=True,
-              help='Creates or updates the Pipfile.lock')
-@click.option('--shell', '-s', is_flag=True,
-              help='Spawns a shell within the virtualenv')
-@click.option('--build-wheel', '-b', is_flag=True,
-              help='Creates a source archive and a wheel for your package')
-@click.option('--test-release', '-t', is_flag=True,
-              help='Uploads the package to test.pypi.org')
-@click.option('--release', '-r', is_flag='True',
-              help='Uploads the package to pypi.org and'
-                   'pushes it to the git remote')
-@click.option('--package', '-p', is_flag=True,
-              help='Creates a new python package from scratch')
-@click.option('--version', '-v', is_flag=True,
-              help='Shows the current version of the package')
-def main(install_all, install, uninstall, dev, lock, shell,
-         build_wheel, test_release, release, package, version):
-    """Executes the entire program
+@click.group()
+def main():
+    """Pybundler is a convinient tool to scaffold python for general purposes,
+    like libraries.
 
-    Parameters \n
-    ----------- \n
-    install_all : bool\n
-        Installs all packages from Pipfile\n
-    install : str\n
-        The given dependency that is going to be installed\n
-    uninstall: str\n
-        Uninstalls the given dependency and removes it from Pipfile\n
-    dev : bool\n
-        Installs the given dependency as a develoment dependency\n
-    lock : bool\n
-        Generates or updates Pipfile.lock\n
-    shell : bool\n
-         Spawns a shell within the virtualenv\n
-    build_wheel : bool\n
-        Creates a source archive and a wheel for your package\n
-    test_release : bool\n
-        Uploads the package to test.pypi.org\n
-    release : bool\n
-        Uploads the package to pypi.org and pushes it to the git remote\n
-    package : bool\n
-        Creates a new python package from scratch\n
-    version : bool\n
-        Shows the current version of the package
+    Type a single command and inmidiately start working with project
+    with all you need.
+    You can select what you need, because this tool ask you before install or
+    create something.
+    We recommend some practices or libraries, but is up to you what wiil
+    be installed.
     """
-    if install_all:
-        cli.install_all(dev)
-
-    if install:
-        cli.install(install, dev)
-
-    if uninstall:
-        cli.uninstall(uninstall)
-
-    if lock:
-        cli.lock()
-
-    if shell:
-        cli.shell()
-
-    if build_wheel:
-        cli.build_source_wheel()
-
-    if test_release:
-        cli.test_release()
-
-    if release:
-        cli.release()
-
-    if package:
-        pkg_name = click.prompt(enter_name())
-        path = click.prompt(enter_path(), default='')
-        license_file = click.confirm(license_option())
-        conduct_file = click.confirm(conduct_option())
-        create_pipenv_env = click.confirm(ask_for_pipenv_env())
-        pytest = click.confirm(install_pytest_confirmation())
-        args = {'name': pkg_name, 'path': path, 'tests': True,
-                'license': license_file, 'code_of_conduct': conduct_file,
-                'pipfile': False}
-        cli.create_pkg(args)
-
-        if create_pipenv_env:
-            python_version = click.prompt(ask_for_python_version(), default="")
-            cli.install_python_version(
-                pkg_dir=os.path.join(path, pkg_name),
-                version=python_version
-            )
-            if pytest:
-                pytest_version = click.prompt(
-                    enter_pytest_version(),
-                    default=''
-                )
-                cli.install_pytest(
-                    os.path.join(path, pkg_name),
-                    pytest_version
-                )
-
-    if version:
-        print(f'pybundler, version {__version__}')
+    pass
 
 
 def conduct_option():
@@ -183,6 +96,75 @@ def ask_for_python_version():
     question = f"Which python version do you need {pyenv_clarification}?"
     default = "(default: the latest python version in the system)"
     return f"{fg(2)} {question} {default} {attr(0)}"
+
+
+@main.command()
+def build_wheel():
+    """Builds the wheel to release the package
+    $ python setup.py sdist bdist_wheel
+    """
+    cli.build_source_wheel()
+
+
+@main.command()
+def test_release():
+    """Uploads the package to test.pypi.org
+    $ twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+    """
+    cli.test_release()
+
+
+@main.command()
+def release():
+    """Uploads the package to pypi.org
+    $ twine upload dist/*
+    """
+    cli.release()
+
+
+@main.command()
+def package():
+    """Creates a new Python package from scratch"""
+    pkg_name = click.prompt(enter_name())
+    path = click.prompt(enter_path(), default='')
+    license_file = click.confirm(license_option())
+    conduct_file = click.confirm(conduct_option())
+    create_pipenv_env = click.confirm(ask_for_pipenv_env())
+    pytest = click.confirm(install_pytest_confirmation())
+    args = {
+        'name': pkg_name,
+        'path': path,
+        'tests': True,
+        'license': license_file,
+        'code_of_conduct': conduct_file,
+        'pipfile': False
+    }
+    cli.create_pkg(args)
+
+    if create_pipenv_env:
+        python_version = click.prompt(
+            ask_for_python_version(),
+            default=""
+        )
+        cli.install_python_version(
+            pkg_dir=os.path.join(path, pkg_name),
+            version=python_version
+        )
+
+        if pytest:
+            pytest_version = click.prompt(
+                enter_pytest_version(),
+                default=''
+            )
+            cli.install_pytest(
+                os.path.join(path, pkg_name),
+                pytest_version
+            )
+
+
+@main.command()
+def version():
+    print(f'pybundler, version {__version__}')
 
 
 if __name__ == '__main__':
